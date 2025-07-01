8abb799d8da88ca2e843aca7068d59256895a1f6
GuptaShubham-11 <shu919847@gmail.com>
2025-07-01 20:54:52 +0530

    add puppetter scraper for tranding topics

---
+++ server/utils/scrapeTrends.ts
@@ -0,0 +1,38 @@
+import puppeteer from "puppeteer";
+
+export const scrapeGoogleTrends = async (): Promise<string[]> => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+
+    try {
+        const page = await browser.newPage();
+
+        if (!page) {
+            throw new Error("Failed to create a new Puppeteer page");
+
+        }
+
+        const response = await page.goto("https://trends.google.com/trending?geo=IN");
+
+        if (!response) {
+            throw new Error("Failed to fetch Google Trends page");
+        }
+
+        const topics = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll(".mZ3RIc"));
+            console.log(elements);
+
+            return elements.map(el => el.textContent?.trim() || "").filter(Boolean);
+        });
+
+
+
+        return topics.slice(0, 10); // Limit to top 10 trends
+    } catch (error) {
+        console.error("Puppeteer error:", error);
+        return [];
+    } finally {
+        await browser.close();
+    }
+};
